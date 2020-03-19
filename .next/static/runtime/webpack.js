69cc214c4cb14e4664081ca898864a0d045dd75b
Roman <romandybinetc@gmail.com>
2020-03-19 18:33:18 +0200

    create pages2

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5ccf742542b5e9b6eea3";
+/******/ 	var hotCurrentHash = "848aef9161846a94fed1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
